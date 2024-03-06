47382ae23d4415ffca59ab95caf2248ca853a874
FerMqz94 <fernando.mqz94@email.com>
2024-03-06 07:29:36 -0300

    Comenzando a implementar multer

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,15 +1,17 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const { uploadProducts } = require('../middlewares/uploadFiles');
 
 // ************ Controller Require ************
-const { create, store, edit, update, destroy } = require('../controllers/admin')
+const { create, store, edit, update, destroy } = require('../controllers/admin');
+
 
 
 // /admin
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/crear-producto', create); 
-router.post('/crear-producto', store); 
+router.post('/crear-producto', uploadProducts.single('img'),store); 
 
 /*** EDIT ONE PRODUCT ***/ 
 router.get('/editar-producto/:id', edit); 
